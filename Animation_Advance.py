bf08a8038ad011612fa207ed2e2bd2cc287307e3
Shubham Tiwari <65031177+ShubhGurukul@users.noreply.github.com>
2021-09-09 23:38:15 +0530

    Basic+Advance Animation
    This is also basic animation but for beginners, this concept is advance like I have used class concepts.

---
+++ Animation_Advance.py
@@ -0,0 +1,30 @@
+import turtle
+import time
+
+turtle.bgcolor('black')
+# pen = turtle.Turtle()
+class Player(turtle.Turtle):
+    def __init__(self) :
+        turtle.Turtle.__init__(self)
+        self.shapesize(5,5,5)
+        self.shape('circle')
+        self.color('deepskyblue')
+        # self.frame = ['circle','square']
+    def animate(self):
+        # self.frame
+        self.shape('square')
+        time.sleep(0.5)
+        self.shape('circle')
+        time.sleep(0.5)
+
+player1 = Player()
+player2 = Player()
+player2.penup()
+player2.goto(200,200)
+
+while True:
+    player1.animate()
+    player2.animate()
+
+
+turtle.mainloop()
